df1fce9a768e06248572f8f047d6d11f0f42f33e
michel.begoc <michel.begoc@f4d3e588-c334-11de-ba9a-9db95b2bc6c5>
2009-11-04 20:15:51 +0000

    Gestion de la langue

--- trunk/action/dao/LanguagesManager.php
+++ trunk/action/dao/LanguagesManager.php
@@ -2,14 +2,37 @@
 require_once("config/config.php");
 
 class LanguagesManager {
+	private $lang;//code langue
+	private $language;//nom complet de la langue
+	private $othersLang = Array();//les autres langues disponibles
+	private $othersLanguage = Array();//autres langues disponibles
+	
 	private $title;
 	private $menu;
 	private $copyright;
 	
+	private $isset = false;//on veut savoir si le langage demandé a bien été trouvé
+	
 	public function __construct($lang = DEFAULT_LANGUAGE) {
+		//initialisation de l'objet
+		$this->init($lang);
+		if(!$this->isset){
+			//si la langue demandée n'a pas été trouvé, il faut reinitialiser les tableaux d'autres langue
+			$this->othersLang = Array();
+			$this->othersLanguage = Array();
+			//puis recommencer avec la langue par défaut
+			$this->init(DEFAULT_LANGUAGE);
+		}
+	}
+
+	public function init($lang){
 		$xml = simplexml_load_file("config/languages.xml");
+		
+		//recherche du langage parmis la liste de langages disponibles
 		foreach ($xml->language as $language) {
 			if($language["lang"] == $lang){
+				$this->lang = $lang;
+				$this->language = $language["fullname"];
 				$this->title = $language->title;
 				$this->copyright = $language->copyright;
 				
@@ -19,10 +42,17 @@ class LanguagesManager {
 				foreach($language->menu->element as $menuElement){
 					$this->menu[$i++] = $menuElement;
 				}
+				
+				//il faut qu'on sache que l'objet est bien initialisé dans le constructeur
+				$this->isset = true;
+			}else{
+				//si on ne trouve pas le langage, on dresse la liste des autres langages disponibles
+				//on en aura besoin pour connaitre les autres langages et les proposée dans la page web
+				$this->othersLang[count($this->othersLang)] = $language["lang"];
+				$this->othersLanguage[count($this->othersLanguage)] = $language["fullname"];
 			}
 		}
 	}
-	
 	public function getTitle() {
 		return $this->title;
 	}
@@ -33,6 +63,22 @@ class LanguagesManager {
 	
 	public function getCopyright(){
 		return $this->copyright;
+	}
+	
+	public function getLang(){
+		return $this->lang;
+	}
+
+	public function getLanguage(){
+		return $this->language;
+	}
+	
+	public function getOthersLang(){
+		return $this->othersLang;
+	}
+
+	public function getOthersLanguage(){
+		return $this->othersLanguage;
 	}
 }
 
