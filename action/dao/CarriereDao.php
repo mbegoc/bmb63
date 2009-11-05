f255860f55951d1ce41dc483c040177b0dd0093b
Massicotte.B <Massicotte.B@f4d3e588-c334-11de-ba9a-9db95b2bc6c5>
2009-11-05 20:40:51 +0000

    CarriereDao fonctionne, scripts updatés et images pour l'équipe.

---
+++ action/dao/CarriereDao.php
@@ -0,0 +1,92 @@
+<?php
+
+	require_once("dao.php");
+	class CarriereDao extends Dao
+	{
+			public $connection;
+			private $result;
+			private $cursor;
+			
+			
+			function __construct()
+			{
+				$this->connection = parent::connecter();
+				$this->cursor = -1;
+			}
+	        public function next()
+	        {
+	        	$values = array();
+	        	$this->cursor++;
+	        	//var_dump($this->result);
+			    
+			    foreach ($this->result as $res)
+			    {
+	        		$values[] = $res[$this->cursor];	
+			    }
+			    return $values;
+	        }
+	        public function previous()
+	        {
+	        	$values = array();
+	        	$this->cursor--;
+	        	
+	        	foreach ($this->result as $res)
+			    {
+	        		$values[] = $res[$this->cursor];	
+			    }
+			    return $values;
+	        }
+	        public function update($id, $colonne , $value)
+	        {
+	        	$query = "update carriere set ".$colonne." = :val where id = :id";
+	        	$statement = oci_parse($this->connection,$query);
+	        	
+	        	oci_bind_by_name($statement , ":id", $id);
+	        	oci_bind_by_name($statement , ":val", $value);
+	        	
+	        	oci_execute($statement);
+	        }
+	        
+	        public function select($listeColonne)
+	        {
+				$query = "SELECT ".$listeColonne." FROM carriere";
+	        	$statement = oci_parse($this->connection,$query);
+	        	oci_execute($statement);
+	        	$this->cursor = -1;
+	        	oci_fetch_all($statement, $this->result);
+				
+	        }
+	        public function insert($tabValues)
+	        {
+	        	$i = 0;
+	        	$query = "insert into carriere values (";
+	        	
+	        	
+	        	foreach ($tabValues as $value)
+	        	{
+	        		$query = $query.":bind".$i.",";
+	        		$i++;
+	        	}
+	        	$query = substr($query,0,-1);
+	        	$query = $query.")";
+	        	
+	        	$statement = oci_parse($this->connection, $query);
+	        	
+	        	$i = 0;
+	        	
+	        	foreach ($tabValues as $value)
+	        	{
+
+	        		oci_bind_by_name($statement , ":bind".$i, $tabValues[$i]);
+	        		$i++;
+	        	}
+	        	
+	        	oci_execute($statement);
+	        		        	
+	        }
+	        
+	        
+		
+
+	        
+	}
--- action/dao/testClasses.php
+++ action/dao/testClasses.php
@@ -1,11 +1,19 @@
 <?php
 	require_once("ServicesDao.php");
-	$service = new ServicesDao;
+	require_once("CarriereDao.php");
+	//$service = new ServicesDao;
 	//$service->select("*"); // select("id,titre")
 	//$service->next();
 	//$service->update(1,"Description","haha"); //id , colonne , valeur
-	
 	//$valueTable = array(15,"titre","fr","des");
 	//$service->insert($valueTable);
+	//$service->deconnecter($service->connection);
 	
-	$service->deconnecter($service->connection);
+	$carriere = new CarriereDao;
+	//$carriere->select("*");
+	//echo $carriere->next();
+	//$carriere->update(1,"description","rien faire");
+	//$carriere->insert(array(10,"fr","danceur","temps plein","Avril 2040"));
+	//$carriere->deconnecter($carriere->connection);
+	
+	
