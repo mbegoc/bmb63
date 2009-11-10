54fa8b814da263e25c4952621ddd9b386ba3a6ce
michel.begoc <michel.begoc@f4d3e588-c334-11de-ba9a-9db95b2bc6c5>
2009-11-10 20:33:18 +0000

    Ajout des fichiers de formulaires + problème css menu réglé

---
+++ forms/servicesForm.php
@@ -0,0 +1,5 @@
+<form action="services.php" method="post">
+	<input type="hidden" name="id" value="" />
+	<textarea name="content"></textarea>
+	<input type="submit" name="submit" value="Envoyer" />
+</form>
