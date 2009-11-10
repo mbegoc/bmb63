19237ac1e1e0e627e7cbec5bed3ae541a9d41cf9
Massicotte.B <Massicotte.B@f4d3e588-c334-11de-ba9a-9db95b2bc6c5>
2009-11-10 22:15:36 +0000

    Gen de contenu Index fonctionne! :D

--- trunk/forms/commonForm.php
+++ trunk/forms/commonForm.php
@@ -1,1 +1,6 @@
-<?php
+<form class = "standardForm" action="index.php" method="post">
+	<input type="hidden" name="id" value="" />
+	<textarea class = "inputStandardForm" name="content"><?php echo $action->getContenu(); ?> </textarea>
+	<input type="submit" name="submit" value="Envoyer" />
+	<input type="submit" name="apercu" value="apercu" />
+</form>
--- trunk/index.php
+++ trunk/index.php
@@ -6,18 +6,40 @@
 * *
 *--------------------------------------------------- */
 require_once("action/IndexAction.php");
+require_once("action/AdminIndexAction.php");
+
+$adminAction = new AdminIndexAction();
 $action = new IndexAction();
 $langManager = $action->execute();
 
 include("header.php");
 
+if (isset($_POST["content"]))
+{
+	$adminAction->setContenu($_POST["content"]);
+}
+
+
 $menu = $langManager->getMenu();
 echo("<h1>" . $menu[0] . "</h1>");
 
 ?>
-	<div>
-		<?php echo $action->getContenu(); ?>
-	</div>
 
-<?php
+	<?php
+	
+	if ($_POST["apercu"] == "apercu")
+	{
+	?>
+		<div>
+			 <?php echo $action->getContenu(); ?>
+		</div>
+	<?php 
+	}
+		else
+		{
+			include("forms/commonForm.php");
+		}
+	
+	
+	
 include("footer.php");
