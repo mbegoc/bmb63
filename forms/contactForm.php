8e0790afadcd1a6880434d23fcb7fd4b8a91bc15
Massicotte.B <Massicotte.B@f4d3e588-c334-11de-ba9a-9db95b2bc6c5>
2009-12-10 17:47:11 +0000

    formulaire contact

--- forms/contactForm.php
+++ forms/contactForm.php
@@ -1,8 +1,9 @@
-<form action="contact.php" method="post">
+<form class = "contactForm" action="contact.php" method="post">
 	<input type="hidden" name="id" value="" />
-	<textarea name="adresse"><?php echo $this->getContactInfo(0)?></textarea>
-	<textarea name="courriel"><?php echo $this->getContactInfo(3)?></textarea>
-	<textarea name="telephone"><?php echo $this->getContactInfo(2)?></textarea>
-	<textarea name="postal"><?php echo $this->getContactInfo(1)?></textarea>
+	<p><span>Adresse</span><textarea name="adresse"><?php echo $this->getContactInfo(0)?></textarea></p>
+	<p><span>Courriel</span><textarea name="courriel"><?php echo $this->getContactInfo(3)?></textarea></p>
+	<p><span>Telephone</span><textarea name="telephone"><?php echo $this->getContactInfo(2)?></textarea></p>
+	<p><span>CodePostal</span><textarea name="postal"><?php echo $this->getContactInfo(1)?></textarea></p>
+	<div class="clear"></div>
 	<input type="submit" name="submit" value="Enregistrer" />
 </form>
