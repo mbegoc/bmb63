67204d168a20b78d526d81e7911962ffaa1f7467
michel.begoc <michel.begoc@f4d3e588-c334-11de-ba9a-9db95b2bc6c5>
2009-12-11 16:35:32 +0000

    Ajustements formulaires, affinage config ckEditor

--- forms/carrieresForm.php
+++ forms/carrieresForm.php
@@ -1,10 +1,13 @@
-<form action="carrieres.php" method="post">
+<form action="carrieres.php" class="editor" method="post">
 	<input type="hidden" name="id" value="<?php echo(isset($carriere) ? $carriere[0] : "");?>" />
+	<p>Titre emploi</p>
 	<input type="text" name="titre" value="<?php echo(isset($carriere) ? $carriere[1] : ""); ?>" />
+	<p>Annonce</p>
 	<textarea name="content"><?php echo(isset($carriere) ? $carriere[2] : ""); ?></textarea>
 	<script type="text/javascript">
-		CKEDITOR.replace('content', {toolbar: "Basic"});
+		CKEDITOR.replace('content', {toolbar: "MyToolbar"});
 	</script>
-	<input type="text" name="date" value="<?php echo(isset($carriere) ? $carriere[3] : ""); ?>" />
+	<p>Date de début</p>
+	<input type="text" name="date" value="<?php echo(isset($carriere) ? $carriere[3] : ""); ?>" /><br />
 	<input type="submit" name="submit" value="<?php echo($action->getLangManager()->getSave()); ?>" />
 </form>
--- forms/commonForm.php
+++ forms/commonForm.php
@@ -1,7 +1,7 @@
 <form class = "standardForm" action="<?php echo $communPageName ?>" method="post">
 	<textarea name="content"><?php echo($action->getContenu()); ?></textarea>
 	<script type="text/javascript">
-		CKEDITOR.replace('content', {toolbar: "Basic"});
+		CKEDITOR.replace('content', {toolbar: "MyToolbar"});
 	</script>
 	<input type="submit" name="apercu" value="<?php echo($action->getLangManager()->getSave()); ?>" />
 </form>
